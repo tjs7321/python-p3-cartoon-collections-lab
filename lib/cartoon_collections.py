83f1281aac8003ea1ffa41c6a22a5c819c20a6a8
Theodore Smith <tjs7321@gmail.com>
2023-08-20 01:54:42 -0400

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+cheeses = ["cheddar", "gouda", "camembert"]
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves):
+    number = 1
+    for dwarf in dwarves:
+        print(f"{number}. {dwarf}")
+        number +=1
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(calls):
+    new_calls = [call.title()+'!' for call in calls]
+    return new_calls
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    big_or_nah = False
+    if len(max(calls, key=len)) > 4:
+        big_or_nah = True
+    return big_or_nah
+
+def find_the_cheese(list):
+    for food in list:
+        if food in cheeses:
+            return food
+    return None
